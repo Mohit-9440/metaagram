651b0fe7d0d3c8d037cf243674e2e26b676e8fad
Mohit <mohitsharma8864@gmail.com>
2022-02-04 23:41:33 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from './constants/routes';
-import UserContext from './context/user';
 import useAuthListener from './hooks/use-auth-listener';
+import UserContext from './context/user';
+import ProtectedRoute from './helpers/protected-routes';
 
 
 const Login = lazy(() => import ('./pages/login'));
@@ -12,7 +13,6 @@ const NotFound = lazy(() => import ('./pages/not-found'));
 
 export default function App() {
   const { user } = useAuthListener();
-
   return (
     <UserContext.Provider value={{ user }}>
       <Router>
@@ -20,7 +20,9 @@ export default function App() {
           <Switch>
             <Route path={ROUTES.LOGIN} component={Login} />
             <Route path={ROUTES.SIGN_UP} component={SignUp} />
-            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+              <Dashboard/>
+            </ProtectedRoute>
             <Route component={NotFound} />
           </Switch>
         </Suspense>
