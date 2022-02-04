405b5aae107b7805ee00ec06acb2bc2337883c88
Mohit <mohitsharma8864@gmail.com>
2022-02-04 23:39:36 +0530

    hooks

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -1,29 +1,22 @@
-import { useState, useEffect, useContext } from 'react';
-import UserContext from '../context/user';
-import { getPhotos, getUserByUserId} from '../services/firebase';
+import { useState, useEffect} from 'react';
+import { getPhotos} from '../services/firebase';
 
-export default function usePhotos() {
-    const [photos, setPhotos] = useState();
-    const {
-        user : { uid: userId = ''} 
-    } = useContext(UserContext);
+export default function usePhotos(user) {
+    const [photos, setPhotos] = useState(null);
 
     useEffect(() => {
         async function getTimelinePhotos() {
-            const [{ following }] = await getUserByUserId(userId);
-            let followedUserPhotos = [];
-
-            // does the user actually follow people?
-            if (following.length > 0) {
-                followedUserPhotos = await getPhotos(userId, following);
+            if (user?.following?.length >= 1) {
+                const followedUserPhotos = await getPhotos(user.userId, user.following);
+                
+                // rearranging array to sort newest photos first by dateCreated
+                followedUserPhotos.sort((a, b) => b.dateCreated - a.dataCreated);
+                setPhotos(followedUserPhotos);
             }
-
-            followedUserPhotos.sort((a, b) => b.dateCreated - a.dataCreated);
-            setPhotos(followedUserPhotos);
         }
 
         getTimelinePhotos();
-    }, [userId]);
+    }, [user.following, user.userId]);
 
     return { photos };
 }
