ff0dddba09a10231387acea5944aa74cfae6df8e
Mohit <mohitsharma8864@gmail.com>
2022-02-04 23:41:07 +0530

    not found page

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
+import Header from '../components/header';
 
 export default function NotFound() {
     useEffect(() => {
-        document.title = 'Not Found - Instagram';
+        document.title = 'Not Found - Metaagram';
     }, []);
     return (
         <div className="bg-gray-background">
+            <Header />
             <div className="mx-auth max-w-screen-lg">
                 <p className="text-center text-2xl">Not Found!</p>
             </div>
